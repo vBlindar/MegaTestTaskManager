6f87dab6d7783f7f8e880187caa3e169bf1aef48
vBlindar <blindar96@mail.ru>
2024-09-09 20:08:55 +0600

    init

---
+++ src/main/java/kg/mega/vblindar/mega_test/MegaTestApplication.java
@@ -0,0 +1,13 @@
+package kg.mega.vblindar.mega_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/mega/vblindar/mega_test/MegaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.mega.vblindar.mega_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
